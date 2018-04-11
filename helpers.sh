7df3c956f588ab4071afc286d14dff87ceaa1d08
elasmo <johan@jails.se>
2018-04-11 13:20:47 +0200

    First

---
+++ helpers.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Various helpers
+#
+
+alias vl='virsh list --all'
+
+if [ "$(pgrep ^spice-vdagent$)" == "" ]; then
+    spice-vdagent
+fi
+
+spiceme() {
+    if [ "$(virsh list --inactive | awk '{print $2}' | grep ^${1}$)" != "" ]; then
+        virsh start ${1}
+    elif [ ! $(virsh dominfo ${1} 2> /dev/null) ]; then
+        echo "Domain '${1}' not found."
+        echo
+        virsh list --all
+        return $?
+    fi
+
+    spicy -f --uri="$(virsh domdisplay $1)"
+}
+
+vmclone() {
+    source="${1}"
+    dest="${2}"
+    diskimage="/var/lib/libvirt/images/${dest}.img"
+
+    if [ -f "${diskimage}" ]; then
+        echo "${diskimage} already exist."
+        return $?
+    fi
+
+    virt-clone -o ${source} -n ${dest} -f /var/lib/libvirt/images/${dest}.img
+}
+
+urlencode() {
+    echo -n $1 | xxd -plain | sed 's/\(..\)/%\1/g'
+}
