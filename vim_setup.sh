2a6be5252d9c573debb68d764d35be129b8ba0af
elasmo <johan@jails.se>
2018-04-29 18:52:36 +0200

    First

---
+++ vim_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Ugly hack for setting up vim. Be careful! ;-)
+#
+
+autoload="${HOME}/.vim/autoload"
+bundle="${HOME}/.vim/bundle"
+vimrc="${HOME}/.vimrc"
+vimrc_remote="https://raw.githubusercontent.com/elasmo/dot-files/master/dot-vimrc"
+
+[ ! -d ${autoload} ] && mkdir -p ${autoload}
+[ ! -d ${bundle} ] && mkdir -p ${autoload}
+[ -e ${vimrc} ] && cp ${vimrc} "${vimrc}.bak"
+
+curl -LSso ${vimrc} ${vimrc_remote}
+[ ! -e ${autoload}/pathogen.vim ] && curl -LSso ${autoload}/pathogen.vim https://tpo.pe/pathogen.vim
+[ ! -d ${bundle}/vim-airline ] && git clone https://github.com/vim-airline/vim-airline ${bundle}/vim-airline
+[ ! -d ${bundle}/vim-airline-themes ] && git clone https://github.com/vim-airline/vim-airline-themes ${bundle}/vim-airline-themes
+[ ! -d ${bundle}/vim-fugitive ] && git clone https://github.com/tpope/vim-fugitive ${bundle}/vim-fugitive
+[ ! -d ${bundle}/vim-colors-solarized ] && git clone https://github.com/altercation/vim-colors-solarized ${bundle}/vim-colors-solarized
