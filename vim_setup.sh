88179093727ac90a0de10a2803ec5791626c8ae9
elasmo <johan@jails.se>
2018-04-30 14:48:39 +0200

    Improved error checking

--- vim_setup.sh
+++ vim_setup.sh
@@ -8,13 +8,13 @@ bundle="${HOME}/.vim/bundle"
 vimrc_local="${HOME}/.vimrc"
 vimrc_remote="https://raw.githubusercontent.com/elasmo/dot-files/master/.vimrc"
 
-[ ! -d ${autoload} ] && mkdir -p ${autoload}
-[ ! -d ${bundle} ] && mkdir -p ${autoload}
-[ -e ${vimrc} ] && cp ${vimrc} "${vimrc}.bak"
+[ ! -d ${autoload} ] && echo "Creating ${autoload}.."; mkdir -p ${autoload}
+[ ! -d ${bundle} ] && echo "Creating ${bundle}.."; mkdir -p ${bundle}
+[ -e ${vimrc} ] && echo "Installing ${vimrc_local}.."; cp ${vimrc_local} "${vimrc}.bak"
 
 curl -Sso ${vimrc_local} ${vimrc_remote}
-[ ! -e ${autoload}/pathogen.vim ] && curl -LSso ${autoload}/pathogen.vim https://tpo.pe/pathogen.vim
-[ ! -d ${bundle}/vim-airline ] && git clone https://github.com/vim-airline/vim-airline ${bundle}/vim-airline
-[ ! -d ${bundle}/vim-airline-themes ] && git clone https://github.com/vim-airline/vim-airline-themes ${bundle}/vim-airline-themes
-[ ! -d ${bundle}/vim-fugitive ] && git clone https://github.com/tpope/vim-fugitive ${bundle}/vim-fugitive
-[ ! -d ${bundle}/vim-colors-solarized ] && git clone https://github.com/altercation/vim-colors-solarized ${bundle}/vim-colors-solarized
+[ ! -e ${autoload}/pathogen.vim ] && echo -n "Installing vim-pathogen.."; curl -LSso ${autoload}/pathogen.vim https://tpo.pe/pathogen.vim || echo "failed"; exit $?
+[ ! -d ${bundle}/vim-airline ] && echo -n "Installing vim-airline.."; git clone https://github.com/vim-airline/vim-airline ${bundle}/vim-airline || echo "failed"; exit $?
+[ ! -d ${bundle}/vim-airline-themes ] && echo -n "Installing vim-airline-themes.."; git clone https://github.com/vim-airline/vim-airline-themes ${bundle}/vim-airline-themes || echo "failed"; exit $?
+[ ! -d ${bundle}/vim-fugitive ] && echo -n "Installing vim-fugitive.."; git clone https://github.com/tpope/vim-fugitive ${bundle}/vim-fugitive || echo "failed"; exit$?
+[ ! -d ${bundle}/vim-colors-solarized ] && "Installing vim-colors-solarized.."; git clone https://github.com/altercation/vim-colors-solarized ${bundle}/vim-colors-solarized || echo "failed"; exit $?
