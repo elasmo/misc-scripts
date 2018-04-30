d21ae97059959cde224f2a0c7193c4f2123adf3b
elasmo <johan@jails.se>
2018-04-30 14:51:12 +0200

    Improved error checking

--- vim_setup.sh
+++ vim_setup.sh
@@ -10,9 +10,9 @@ vimrc_remote="https://raw.githubusercontent.com/elasmo/dot-files/master/.vimrc"
 
 [ ! -d ${autoload} ] && echo "Creating ${autoload}.."; mkdir -p ${autoload}
 [ ! -d ${bundle} ] && echo "Creating ${bundle}.."; mkdir -p ${bundle}
-[ -e ${vimrc} ] && echo "Installing ${vimrc_local}.."; cp ${vimrc_local} "${vimrc}.bak"
+[ -e ${vimrc} ] && echo "Backing up ${vimrc_local} to ${vimrc_local}.bak.."; cp ${vimrc_local} "${vimrc_local}.bak"
 
-curl -Sso ${vimrc_local} ${vimrc_remote}
+echo -n "Installing ${vimrc_local} from ${vimrc_remote}"; curl -Sso ${vimrc_local} ${vimrc_remote}; echo || echo "failed"; exit $?
 [ ! -e ${autoload}/pathogen.vim ] && echo -n "Installing vim-pathogen.."; curl -LSso ${autoload}/pathogen.vim https://tpo.pe/pathogen.vim || echo "failed"; exit $?
 [ ! -d ${bundle}/vim-airline ] && echo -n "Installing vim-airline.."; git clone https://github.com/vim-airline/vim-airline ${bundle}/vim-airline || echo "failed"; exit $?
 [ ! -d ${bundle}/vim-airline-themes ] && echo -n "Installing vim-airline-themes.."; git clone https://github.com/vim-airline/vim-airline-themes ${bundle}/vim-airline-themes || echo "failed"; exit $?
