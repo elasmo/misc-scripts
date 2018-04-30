e485b7bc2fda0f46a05c3101818412d180aee37a
elasmo <johan@jails.se>
2018-04-30 14:33:54 +0200

    Changed vimrc remote path.

--- vim_setup.sh
+++ vim_setup.sh
@@ -5,14 +5,14 @@
 
 autoload="${HOME}/.vim/autoload"
 bundle="${HOME}/.vim/bundle"
-vimrc="${HOME}/.vimrc"
-vimrc_remote="https://raw.githubusercontent.com/elasmo/dot-files/master/dot-vimrc"
+vimrc_local="${HOME}/.vimrc"
+vimrc_remote="https://raw.githubusercontent.com/elasmo/dot-files/master/.vimrc"
 
 [ ! -d ${autoload} ] && mkdir -p ${autoload}
 [ ! -d ${bundle} ] && mkdir -p ${autoload}
 [ -e ${vimrc} ] && cp ${vimrc} "${vimrc}.bak"
 
-curl -Sso ${vimrc} ${vimrc_remote}
+curl -Sso ${vimrc_local} ${vimrc_remote}
 [ ! -e ${autoload}/pathogen.vim ] && curl -LSso ${autoload}/pathogen.vim https://tpo.pe/pathogen.vim
 [ ! -d ${bundle}/vim-airline ] && git clone https://github.com/vim-airline/vim-airline ${bundle}/vim-airline
 [ ! -d ${bundle}/vim-airline-themes ] && git clone https://github.com/vim-airline/vim-airline-themes ${bundle}/vim-airline-themes
