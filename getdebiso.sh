d22fc037dba7792b66568de2ea7d56f5bd2bb24f
elasmo <elasmo@github>
2018-06-18 09:42:04 +0200

    Improved error checking

--- getdebiso.sh
+++ getdebiso.sh
@@ -13,25 +13,30 @@ iso_url="${base}/${iso_file}"
 sha_url="${base}/SHA512SUMS"
 sig_url="${base}/SHA512SUMS.sign"
 
-which curl > /dev/null || { echo >&2 "error: curl not found"; exit 1; }
-which gpg > /dev/null || { echo >&2 "error: gpg not found"; exit 1; }
-which openssl > /dev/null || { echo >&2 "error: openssl not found"; exit 1; }
-which dirmngr > /dev/null || { echo >&2 "error: dirmngr not found"; exit 1; }
+error() {
+    echo >&2 "$@"
+    exit 1
+}
+
+which curl > /dev/null || error "curl not found" 
+which gpg > /dev/null || error "gpg not found"
+which openssl > /dev/null || error "openssl not found"
+which dirmngr > /dev/null || error "dirmngr not found"
 
 if [ ! -f ${iso_file} ]; then
     echo -n "=== Fetching ${iso_url}..."
-    curl -sLO "${iso_url}" && echo "OK" || { echo >&2 "failed"; exit 1; }
+    curl -sLO "${iso_url}" && echo "OK" || error "failed"
 else 
     echo "=== Found ${iso_file}"
 fi
 
 echo -n "=== Verifying hash..."
-curl -sO "${sha_url}" || { echo >&2 "fetch failed"; exit 1; }
-expected_hash=$(grep -E "^[0-9a-f]{128}\s{2}${iso_file}$" ${sha_file} | awk '{print $1}' || { echo >&2 "failed"; exit 1; })
+curl -sO "${sha_url}" || error "fetch failed"
+expected_hash=$(grep -E "^[0-9a-f]{128}\s{2}${iso_file}$" ${sha_file} | awk '{print $1}' || error "failed")
 iso_hash=$(openssl sha512 "${iso_file}" | cut -f2 -d' ')
-[ "${iso_hash}" = "${expected_hash}" ] && echo "OK" || { echo >&2 "failed"; exit 1; }
+[ "${iso_hash}" = "${expected_hash}" ] && echo "OK" || error "hash mismatch"
 
 echo -n "=== Verifying signature..."
-gpg --keyserver keyring.debian.org --recv 6294BE9B 2> /dev/null || { echo >&2 "fetch key failed"; exit 1; }
-curl -sO "${sig_url}" || { echo >&2 "fetch signature failed"; exit 1; }
-gpg --verify ${sig_file} ${sha_file} 2> /dev/null && echo "OK" || { echo >&2 "verification failed"; exit 1; }
+gpg --keyserver keyring.debian.org --recv 6294BE9B 2> /dev/null || error "fetch key failed"
+curl -sO "${sig_url}" || error "fetch signature failed"
+gpg --verify ${sig_file} ${sha_file} 2> /dev/null && echo "OK" || error "verification failed"
