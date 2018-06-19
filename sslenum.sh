23a0edfc3542ec623dc8b42f62378239a9b8d92e
elasmo <elasmo@github>
2018-06-19 11:01:59 +0200

    Minor edits.

--- sslenum.sh
+++ sslenum.sh
@@ -9,7 +9,7 @@ ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
 [ "$#" -gt 0 ] || { echo >&2 "Usage: $0 <server> [port]"; exit 1; }
 [ -n "$2" ] || port=443
 
-for cipher in ${ciphers[@]}; do
+for cipher in ${ciphers}; do
     resp=$(echo -n | openssl s_client -cipher "${cipher}" -connect ${server}:${port} 2>&1)
     if [[ ! "${resp}" =~ ":error:" ]] ; then
         if [[ "${resp}" =~ "Cipher is ${cipher}" || "${resp}" =~ "Cipher    :" ]] ; then
