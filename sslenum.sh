50c0b5c012591effa2a2f25d0c7a4bf2f5ee2ae5
elasmo <elasmo@github>
2018-06-19 10:22:35 +0200

    First

---
+++ sslenum.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Enumerates supported cipher suites
+#
+server=$1
+port=$2
+ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
+
+error() {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -gt 0 ] || error "Usage: $0 <server> [port]"
+[ -n "$2" ] || port=443
+
+which openssl > /dev/null || error "openssl not found"
+
+echo "=== Enumerating supported cipher suites for ${server}:${port}"
+
+for cipher in ${ciphers[@]}; do
+    resp=$(echo -n | openssl s_client -cipher "${cipher}" -connect ${server}:${port} 2>&1)
+    if [[ ! "${resp}" =~ ":error:" ]] ; then
+        if [[ "${resp}" =~ "Cipher is ${cipher}" || "${resp}" =~ "Cipher    :" ]] ; then
+            echo ${cipher}
+        fi
+    fi
+done
