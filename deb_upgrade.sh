10610d3cf4a3d6685e8851d62403264d4f6f304e
elasmo <johan@jails.se>
2018-07-19 14:31:02 +0200

    Added more strict rules

--- deb_upgrade.sh
+++ deb_upgrade.sh
@@ -12,23 +12,36 @@ fi
 
 trap close_ports EXIT
 ports="443 80"
+hosts=(
+    "ftp.se.debian.org"
+    "security.debian.org" 
+    "gemmei.ftp.acc.umu.se"
+    "caesar.ftp.acc.umu.se"
+    "saimei.ftp.acc.umu.se"
+    "security-cdn.debian.org"
+    "gensho.ftp.acc.umu.se"
+)
 
 close_ports() {
-    for port in ${ports}; do
-        echo "==> Closing ${port}"
-        iptables -D INPUT -i eth0 -m state -p tcp --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
-        iptables -D OUTPUT -o eth0 -p tcp --dport ${port} -j ACCEPT
+    for port in ${ports}; do 
+        for host in ${hosts[@]}; do
+            echo "==> Closing ${host}:${port}"
+            iptables -D INPUT -i eth0 -m state -p tcp -s ${host} --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
+            iptables -D OUTPUT -o eth0 -p tcp -d ${host} --dport ${port} -j ACCEPT
+        done
     done
 }
 
 for port in ${ports}; do
-    echo "==> Opening ${port}"
-    iptables -A INPUT -i eth0 -m state -p tcp --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
-    iptables -A OUTPUT -o eth0 -p tcp --dport ${port} -j ACCEPT
+    for host in ${hosts[@]}; do
+        echo "==> Opening ${host}:${port}"
+        iptables -A INPUT -i eth0 -m state -p tcp -s ${host} --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
+        iptables -A OUTPUT -o eth0 -p tcp -d ${host} --dport ${port} -j ACCEPT
+    done
 done
 
 echo "==> Updating local repository"
-apt -y update || error "update failed"                                                                                                                                                         
+apt -y update || error "update failed"
 
 echo "==> Performing dist upgrade"
 apt -y dist-upgrade || error "dist-upgrade failed"
