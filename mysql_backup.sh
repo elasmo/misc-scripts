e8a5c47d6236ff84bfb4893755614be8841fced9
elasmo <johan@jails.se>
2018-07-19 17:18:56 +0200

    Improved

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,61 +1,29 @@
-#!/bin/sh
-#
-# 2017
+#!/usr/bin/env bash
+set -euo pipefail
+
+export PATH="${PATH}:/usr/local/bin"
+
 script_name="$(basename $0)"
+user=""
+password=""
+host=""
+perms=600
 
-mysql_user="user"
-mysql_password="pass"
-mysql_host="host"
+databases=(
+    "db1"
+    "db2"
+)
 
-db_ignore="Database information_schema performance_schema sys"
-mysql_dump="/usr/local/bin/mysqldump"
-mysql_bin="/usr/local/bin/mysql"
-dump_perms=644
+error() {
+    logger -t ${script_name} "$@ $-"
+    exit 1
+}
 
-db_list="$(${mysql_bin} -h ${mysql_host} -u ${mysql_user} \
-         -p${mysql_password} -e 'show databases')"
-
-if [ $? -ne 0 ]
-then
-    logger -t ${script_name} "Unable to list databases."
-    exit 1;
-fi
-
-for db in ${db_list}
-do
-	ignored=0
-    for i in ${db_ignore}
-    do
-        if [ "${db}" == "${i}" ]
-        then
-            echo "Ignoring database '${db}'."
-            ignored=1
-        fi
-    done
-
-    if [ ${ignored} -eq 1 ]
-    then
-        continue
-    fi
-            
-    dump_file="${db}.sql"
-    ${mysql_dump} -u${mysql_user} -p${mysql_password} -h${mysql_host} \
-        "${db}" > "${dump_file}"
-
-    if [ $? -ne 0 ]
-    then
-        logger -t ${script_name} "Unable to dump database '${db}'."
-        exit 1
-    fi
-    chmod ${dump_perms} ${dump_file}
-    bzip2 ${dump_file}
+for database in ${databases[@]}; do
+    db_dump="${database}.sql"
+    mysqldump -u ${user} -p${password} -h ${host} ${database} > $db_dump || error "Database error while working on ${database}"
+    chmod ${perms} ${db_dump}
+    bzip2 ${db_dump} || error "Compressing ${db_dump} failed"
 done
 
-if [ $? -eq 0 ]
-then
-    logger -t ${script_name} "MySQL backup sucessful."
-    exit 0
-else
-    logger -t ${script_name} "Unknown error."
-    exit 1
-fi
+logger -t ${script_name} "MySQL backup sucessful"
