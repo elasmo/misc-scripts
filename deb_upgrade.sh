4bd1940cd408afd37449436572618653a78838c1
elasmo <johan@jails.se>
2018-07-20 12:20:10 +0200

    Pretty printing

--- deb_upgrade.sh
+++ deb_upgrade.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-error() {
-    echo >&2 "$@"
-    exit 1
-}
+trap close_ports EXIT
 
 if [ $(whoami) != "root" ]; then
     error "$(whoami) are not welcome here!"
 fi
 
-trap close_ports EXIT
 ports="443 80"
 hosts=(
     "ftp.se.debian.org"
@@ -22,10 +17,19 @@ hosts=(
     "gensho.ftp.acc.umu.se"
 )
 
+error() {
+    printf >&2 "\e[38;5;155m===>\e[0m\e[0;31m $@\e[0m\n"
+    exit 1
+}
+
+pprint() {
+    printf "\e[38;5;155m===>\e[0m $@\n"
+}
+
 close_ports() {
     for port in ${ports}; do 
         for host in ${hosts[@]}; do
-            echo "==> Closing ${host}:${port}"
+            pprint "Closing ${host}:${port}"
             iptables -D INPUT -i eth0 -m state -p tcp -s ${host} --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
             iptables -D OUTPUT -o eth0 -p tcp -d ${host} --dport ${port} -j ACCEPT
         done
@@ -34,18 +38,18 @@ close_ports() {
 
 for port in ${ports}; do
     for host in ${hosts[@]}; do
-        echo "==> Opening ${host}:${port}"
+        pprint "Opening ${host}:${port}"
         iptables -A INPUT -i eth0 -m state -p tcp -s ${host} --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
         iptables -A OUTPUT -o eth0 -p tcp -d ${host} --dport ${port} -j ACCEPT
     done
 done
 
-echo "==> Updating local repository"
+pprint "Updating local repository"
 apt -y update || error "update failed"
 
-echo "==> Performing dist upgrade"
+pprint "Performing dist upgrade"
 apt -y dist-upgrade || error "dist-upgrade failed"
 
-echo "==> Cleaning up"
+pprint "Cleaning up"
 apt autoremove || error "autoremove failed"
 apt clean || error "clean failed"
--- fbsd_update.sh
+++ fbsd_update.sh
@@ -10,43 +10,43 @@ jail_root="/jail"
 jails="jail1 jail2 jail3 jail4"
 local_machine="$(hostname -s)"
 
-pecho() {
+pprint() {
     printf "\e[0;35m===>\e[0m $@\n"
 }
 
-pecho "Updating local repository on ${local_machine}"
+pprint "Updating local repository on ${local_machine}"
 pkg update 
 
-pecho "Upgrading packages on ${local_machine}"
+pprint "Upgrading packages on ${local_machine}"
 pkg upgrade
 
-pecho "Removing stale packages on ${local_machine}"
+pprint "Removing stale packages on ${local_machine}"
 pkg autoremove -y
 
-pecho "Removing package cache on ${local_machine}"
+pprint "Removing package cache on ${local_machine}"
 pkg clean -ay
 
-pecho "Installing base and kernel updates on ${local_machine}"
+pprint "Installing base and kernel updates on ${local_machine}"
 PAGER=cat freebsd-update --not-running-from-cron fetch install
 
 for jail in ${jails}; do
-    pecho "Updating local repository on ${jail}"
+    pprint "Updating local repository on ${jail}"
     pkg -j ${jail} update
 
-    pecho "Upgrading packages on ${jail}"
+    pprint "Upgrading packages on ${jail}"
     pkg -j ${jail} upgrade
 
-    pecho "Removing stale packages on ${jail}"
+    pprint "Removing stale packages on ${jail}"
     pkg -j ${jail} autoremove -y
 
-    pecho "Removing package cache on ${jail}"
+    pprint "Removing package cache on ${jail}"
     pkg -j ${jail} clean -ay
 
-    pecho "Installing base and kernel updates on ${jail}"
+    pprint "Installing base and kernel updates on ${jail}"
     PAGER=cat freebsd-update -b "${jail_root}/${jail}" --not-running-from-cron fetch install
 done
 
-pecho "Note:"
+pprint "Note:"
 echo "- ${jail_root}/base is not included in this upgrade"
 echo "- if any kernel updates has been included, the kernel needs to be rebuilt using"
 echo "  the custom confiugration:"
