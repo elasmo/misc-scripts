2a6ff57ee3e097894b28fde3f4f224ebeb21c4c3
elasmo <elasmo@github>
2018-07-24 14:33:31 +0200

    Using env for password instead

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 export PATH="${PATH}:/usr/local/bin"
+export MYSQL_PWD=""
 
 script_name="$(basename $0)"
 user=""
-password=""
 host=""
 perms=600
 
@@ -21,7 +21,7 @@ error() {
 
 for database in ${databases[@]}; do
     db_dump="${database}.sql"
-    mysqldump -u ${user} -p${password} -h ${host} ${database} > $db_dump || error "Database error while working on ${database}"
+    mysqldump -u ${user} -h ${host} ${database} > $db_dump || error "Database error while working on ${database}"
     chmod ${perms} ${db_dump}
     bzip2 ${db_dump} || error "Compressing ${db_dump} failed"
 done
