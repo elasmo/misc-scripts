33d37d90996888a48a1fc47f2089b7a4ac20e9a6
Charlie Root <root@shiner.bluebyte.lan>
2018-08-23 13:53:49 +0200

    Initially clear blacklist & update table afterwards

--- geoip_blacklist.sh
+++ geoip_blacklist.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 # 
-# Generates a blacklist used for GeoIP blocking using OpenBSD pf
-#                                                                                              
+# Generates a blacklist used  for GeoIP blocking using OpenBSD pf
+#
 # pf.conf example:
 # table <geoip_blacklist> persist file "/etc/pf/geoip_blacklist"
 # block in quick on egress from <geoip_blacklist> 
 #
 # https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+#
 country_codes="cn az by kz kg ru tj tm uz vn"
-out_file="/etc/pf/geoip_blacklist"
+pf_dir="/etc/pf"
+pf_table="geoip_blacklist"
+blacklist="${pf_dir}/geoip_blacklist"
+
+echo > ${blacklist}
+chmod 600 ${blacklist}
 
 for cc in ${country_codes}; do 
-    ftp -o - http://ipdeny.com/ipblocks/data/countries/${cc}.zone >> ${out_file}
+    ftp -o - http://ipdeny.com/ipblocks/data/countries/${cc}.zone >> ${blacklist}
+    sleep 1
 done
 
-chmod 600 ${out_file}
+pfctl -t ${pf_table} -T replace -f ${blacklist}
+
+logger "${blacklist} blacklist updated."
