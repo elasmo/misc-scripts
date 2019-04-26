87b3400ea3effe9c9221bd895c1c001f978ceaf2
elasmo <elasmo@github>
2019-04-26 13:15:30 +0200

    determine default route nic, instead of open ports on all nics

--- deb_upgrade.sh
+++ deb_upgrade.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ports="443 80"
-hosts=(
+error() {
+    printf >&2 "\e[38;5;155m===>\e[0m\e[0;31m $@\e[0m\n"
+    exit 1
+}
+
+if [ $(id -u) -ne 0 ]; then
+    error "$(whoami) are not welcome here!"
+fi
+
+apt_options="--no-install-recommends -qq --yes"
+ports="443,80"
+net_if=$(ip -4 route list 0/0 | cut -f5 -d' ')
+
+# Note: add these to /etc/hosts
+repositories=(
     "ftp.se.debian.org"
     "security.debian.org" 
     "gemmei.ftp.acc.umu.se"
@@ -12,45 +25,32 @@ hosts=(
     "gensho.ftp.acc.umu.se"
 )
 
-error() {
-    printf >&2 "\e[38;5;155m===>\e[0m\e[0;31m $@\e[0m\n"
-    exit 1
-}
-
 pprint() {
     printf "\e[38;5;155m===>\e[0m $@\n"
 }
 
 close_ports() {
-    for port in ${ports}; do 
-        for host in ${hosts[@]}; do
-            pprint "Closing ${host}:${port}"
-            iptables -D INPUT -i eth0 -m state -p tcp -s ${host} --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
-            iptables -D OUTPUT -o eth0 -p tcp -d ${host} --dport ${port} -j ACCEPT
-        done
+    pprint "Closing ports"
+    for repo in ${repositories[@]}; do
+        iptables -D OUTPUT -o ${net_if} -p tcp -d ${repo} -m multiport --dports ${ports} -j ACCEPT
+        iptables -D INPUT -i ${net_if} -m state -p tcp -s ${repo} -m multiport --sports ${ports} --state ESTABLISHED,RELATED -j ACCEPT
     done
 }
 
-if [ $(whoami) != "root" ]; then
-    error "$(whoami) are not welcome here!"
-fi
-
 trap close_ports EXIT
 
-for port in ${ports}; do
-    for host in ${hosts[@]}; do
-        pprint "Opening ${host}:${port}"
-        iptables -A INPUT -i eth0 -m state -p tcp -s ${host} --sport ${port} --state ESTABLISHED,RELATED -j ACCEPT
-        iptables -A OUTPUT -o eth0 -p tcp -d ${host} --dport ${port} -j ACCEPT
-    done
+pprint "Opening ports"
+for repo in ${repositories[@]}; do
+    iptables -A OUTPUT -o ${net_if} -p tcp -d ${repo} -m multiport --dports ${ports} -j ACCEPT
+    iptables -A INPUT -i ${net_if} -m state -p tcp -s ${repo} -m multiport --sports ${ports} --state ESTABLISHED,RELATED -j ACCEPT
 done
 
-pprint "Updating local repository"
-apt -y update || error "apt update failed"
+pprint "Updating local repo"
+apt-get ${apt_options} update || error "update failed"
 
-pprint "Performing dist upgrade"
-apt -y dist-upgrade || error "apt dist-upgrade failed"
+pprint "Performing upgrade"
+apt-get ${apt_options} dist-upgrade || error "dist-upgrade failed"
 
 pprint "Cleaning up"
-apt autoremove || error "autoremove failed"
-apt clean || error "clean failed"
+apt-get ${apt_options} autoremove || error "autoremove failed"
+apt-get ${apt_options} clean || error "clean failed"
