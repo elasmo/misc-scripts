243ff10d3e4ceaaacb0460dd17abea3d16e612a0
elasmo <elasmo@github>
2020-05-25 19:36:16 +0200

    openbsd support

--- getdebiso.sh
+++ getdebiso.sh
@@ -2,42 +2,59 @@
 #
 # Fetches debian iso, verifies sha512 hash and signature
 #
-#base="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd"
+
+cleanup() {
+    rm -f $TMP_SIG $TMP_SHA
+}
 
 error() {
     echo >&2 "$@"
+    cleanup
     exit 1
 }
 
-if [ $# != 1 ]; then
-    error "Usage: $(basename $0) <url>"
-fi
+[ $# != 1 ] && error "Usage: $(basename $0) <url>"
+
+get_url() {
+    [ -s $2 ] && return
+    echo "Requesting $1"
+    curl -fsL $1 -o $2
+}
+
+trap cleanup EXIT 
 
 iso_url="$1"
 base_url="${iso_url%/*}"
 iso_file="${iso_url##*/}"
-key_id="6294BE9B"
-TMP_SHA512=$(mktemp)
+sha_url="${base_url}/SHA512SUMS"
+sig_url="${base_url}/SHA512SUMS.sign"
+TMP_SHA=$(mktemp)
 TMP_SIG=$(mktemp)
+key_id="6294BE9B"
 
-which curl > /dev/null || error "curl not found" 
-which openssl > /dev/null || error "openssl not found"
-which dirmngr > /dev/null || error "dirmngr not found"
-gpg_runtime=$(which gpg 2> /dev/null || which gpgv2 2> /dev/null) || error "gpg not found"
-
+# Check dependencies
+for dep in curl openssl dirmngr; do
+    type $dep > /dev/null || error "$dep not found"
+done
+type gpg > /dev/null || type gpg2 > /dev/null || error "gpg not found"
+   
 # Download ISO
-if [ ! -f ./${iso_file} ]; then
-    echo "Requesting ${iso_url}..."
-    curl -fsLO "${iso_url}" && echo "OK" || error "Error retrieving ${iso_url}"
-else 
-    echo "Found $PWD/${iso_file}"
-fi
+get_url $iso_url $iso_file
 
 # Verify SHA512 hash
-curl -fsO "${base_url}/SHA512SUMS" -o $TMP_SHA512 || error "fetch failed"
-sha512 -C SHA512SUMS "${iso_file}" || error "hash mismatch"
+get_url "$sha_url" $TMP_SHA
+sha512 -C $TMP_SHA "$iso_file" || error "Hash mismatch"
 
 # Verify signature
-${gpg_runtime} --keyserver keyring.debian.org --recv ${key_id}
-curl -O "${base_url}/SHA512SUMS.sign" -o $TMP_SIG || error "fetch signature failed"
-${gpg_runtime} --verify $TMP_SIG $TMP_SHA512 2> /dev/null && echo "OK" || error "verification failed"
+get_url "$sig_url" $TMP_SIG
+
+case "$(uname -s)" in
+    "OpenBSD") 
+        gpg2 --search-keys $key_id || error "Error retrieving $key_id"
+        echo $TMP_SIG $iso_file
+        gpg2 --verify $TMP_SIG $TMP_SHA || error "Verification failed";;
+    "Linux") 
+        gpg --keyserver --recv $key_id || error "Error retrieving $key_id"
+        gpg --verify $TMP_SIG $TMP_SHA
+        ;;
+esac
