6d3d8d7d57dec4c7309bd5888fd3e4e24f50bf9b
elasmo <elasmo@github>
2020-06-22 13:42:57 +0200

    Mod

---
+++ urldecode.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if ! type xxd >/dev/null; then
+    echo "xxd not found"
+    exit 1
+fi
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $(basename $0) <string>"
+    exit 1
+fi
+
+url="0:$(echo -n "$@" | sed 's/%/ /g')"
+echo $url | xxd -r
+
+#echo -n $1 | xxd -plain | sed 's/\(..\)/%\1/g'
