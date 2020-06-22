2c415308b59c5421e10c9231ec03cd0b9ebfaa3d
elasmo <elasmo@github>
2020-06-22 15:34:35 +0200

    Removed -w since it's Linux only

--- token_urlsafe.sh
+++ token_urlsafe.sh
@@ -2,7 +2,5 @@
 #
 # Generate URL safe random tokens
 #
-_bytes=32
 [ $# -eq 1 ] && _bytes=$1 || _bytes=32
-dd if=/dev/urandom bs=$_bytes count=1 2>/dev/null | base64 -w 0 | tr '+/' '-_' | tr -d  '='
-echo
+dd if=/dev/urandom bs=$_bytes count=1 2>/dev/null | base64 | tr '+/' '-_' | tr -d  '='
