8c696328f59e87b450eb6861d6688d3b02058fae
elasmo <elasmo@github>
2020-06-22 13:38:51 +0200

    removed

--- helpers.sh
+++ helpers.sh
@@ -6,12 +6,3 @@ fi
 snapshot() {
     virsh snapshot-create-as --domain "$1" --name "snapshot-$(date +%s)"
 }
-
-urlencode() {
-    echo -n $1 | xxd -plain | sed 's/\(..\)/%\1/g'
-}
-
-urldecode() {
-    url="0:$(echo -n "$@" | sed 's/%/ /g')"
-    echo $url | xxd -r
-}
