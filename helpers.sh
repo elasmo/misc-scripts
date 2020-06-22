152ecde5ad48af009820766a4d82eebc81f1a4c2
elasmo <elasmo@github>
2020-06-22 16:48:34 +0200

    Removed

--- helpers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if [ "$(pgrep ^spice-vdagent$)" == "" ]; then
-    spice-vdagent
-fi
-
-snapshot() {
-    virsh snapshot-create-as --domain "$1" --name "snapshot-$(date +%s)"
-}
