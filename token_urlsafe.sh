c3c0e95374c68f262bd8f5b94180dc9eeb6f5348
elasmo <elasmo@github>
2020-06-22 16:18:28 +0200

    Use openssl instead

--- token_urlsafe.sh
+++ token_urlsafe.sh
@@ -3,4 +3,5 @@
 # Generate URL safe random tokens
 #
 [ $# -eq 1 ] && _bytes=$1 || _bytes=32
-dd if=/dev/urandom bs=$_bytes count=1 2>/dev/null | base64 | tr '+/' '-_' | tr -d  '='
+#dd if=/dev/urandom bs=$_bytes count=1 2>/dev/null | base64 | tr '+/' '-_' | tr -d  '='
+openssl rand -base64 $_bytes | tr '+/' '-_' | tr -d '='
