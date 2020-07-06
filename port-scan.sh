ffe31789f0fe1568f1c2e270b640d17890e124b2
elasmo <elasmo@github>
2020-07-06 16:32:55 +0200

    Changed description

--- port-scan.sh
+++ port-scan.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #
-# Simple one-liner portscan
+# Simple one-liner TCP portscan
 
 while read port; do </dev/tcp/127.0.0.1/${port} && echo ${port}; done < <(grep -oE [0-9]+/tcp /etc/services | tr -d '/tcp')
