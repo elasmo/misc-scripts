ee1364ae4762d807dbea1300c80cbc5be7053ea5
elasmo <elasmo>
2022-08-02 13:11:34 +0200

    Add

--- add-burp-cert.sh
+++ add-burp-cert.sh
@@ -3,22 +3,22 @@
 # Simple helper to add burp certificate to Androids system trust store
 #
 set -e
-system_store="/system/etc/security/cacerts/"
-cert_backup="/data/local/tmp/certs.bak"
+truststore="/system/etc/security/cacerts/"
+truststore_copy="/data/local/tmp/certs.bak"
 tmp="/data/local/tmp"
 
 curl -s -O localhost:8080/cert
-new_filename=$(openssl x509 -inform der -subject_hash -in cert | head  -1).0
-openssl x509 -inform der -in cert -out ${new_filename}
+burp_cert=$(openssl x509 -inform der -subject_hash -in cert | head  -1).0
+openssl x509 -inform der -in cert -out ${burp_cert}
 
-adb shell su -c mkdir -p ${cert_backup}
-adb shell su -c cp ${system_store}/* ${cert_backup}
-adb shell su -c mount -t tmpfs tmpfs ${system_store}
-adb shell su -c mv ${cert_backup}/* ${system_store}
-adb push ${new_filename} ${tmp}
-adb shell su -c mv ${tmp}/${new_filename} ${system_store}
-adb shell su -c chown root:root ${system_store}/*
-adb shell su -c chmod 644 ${system_store}/*
-adb shell su -c chcon u:object_r:system_file:s0 ${system_store}/*
+adb shell su -c mkdir -p ${truststore_copy}
+adb shell su -c cp ${truststore}/* ${truststore_copy}
+adb shell su -c mount -t tmpfs tmpfs ${truststore}
+adb shell su -c mv ${truststore_copy}/* ${truststore}
+adb push ${burp_cert} ${tmp}
+adb shell su -c mv ${tmp}/${burp_cert} ${truststore}
+adb shell su -c chown root:root ${truststore}/*
+adb shell su -c chmod 644 ${truststore}/*
+adb shell su -c chcon u:object_r:system_file:s0 ${truststore}/*
 
-rm cert ${new_filename}
+rm cert ${burp_cert}
