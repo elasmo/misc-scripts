a709a69706f73bc43d64b0fce81a059d81927364
elasmo <elasmo@local>
2022-12-18 22:11:23 +0100

    Minor fix

--- colors.sh
+++ colors.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+bold="\e[1m"
+uline="\e[4m"
+rev="\e[7m"
+reset="\e[0m"
 
-for c in {1..256}; do
-    printf "\e[38;5;${c}m${c}\e[0m\t"
+for c in {0..255}; do
+    printf "\e[38;5;%sm%s\e[0m\t" ${c} ${c}
+	printf "\e[1m"
+    printf "\e[38;5;%sm%s${reset}\t" ${c} ${c}
 
-    if [ $(($c % 10)) -eq 0 ]; then
-        echo
-    fi
+	[ $((${c}%10)) -eq 0 ] && printf "\n"
 done
 
 echo
